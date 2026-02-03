151db4b2c5356e424d98fb61a288f3b9a3ffee49
Skeityt1 <kminh1648@gmail.com>
2026-02-04 00:40:56 +0700

    init

--- static/js/main.ce7c4279.chunk.js
+++ static/js/main.ce7c4279.chunk.js
@@ -265,7 +265,7 @@ if (!self.__WB_pmw) {
                               h.globalCompositeOperation = "source-over",
                               0,
                               x(0),
-                              DO = d || "",
+                              var DO = d || "",
                               h.font = "90px Nanum Pen Script",
                               W = k.measureText(DO).width,
                               p = 1530 - W / 2,
